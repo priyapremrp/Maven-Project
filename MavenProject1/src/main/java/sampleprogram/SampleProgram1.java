10f62a173c78d3234dc245885c1175ea97e8a4c2
TOSHIBA <TOSHIBA@DESKTOP-3HMAMIJ>
2024-03-27 20:32:57 +0530

    Sample Program

---
+++ MavenProject1/src/main/java/sampleprogram/SampleProgram1.java
@@ -0,0 +1,19 @@
+package sampleprogram;
+
+public class SampleProgram1 
+{
+
+	public void display()
+	{
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		SampleProgram1 obj = new SampleProgram1();
+		obj.display();
+
+	}
+
+}
