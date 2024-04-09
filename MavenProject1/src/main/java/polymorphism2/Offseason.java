a10335fa14a488241c72ec2d2bc2d1ce4aa02cd5
TOSHIBA <TOSHIBA@DESKTOP-3HMAMIJ>
2024-04-09 20:43:14 +0530

    stage

--- MavenProject1/src/main/java/polymorphism2/Offseason.java
+++ MavenProject1/src/main/java/polymorphism2/Offseason.java
@@ -1,13 +1,35 @@
 package polymorphism2;
 
+import java.util.Scanner;
+
 public class Offseason extends Onseason
 {
 	
 	public void discount()
 	{
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Choose a season:");
+		System.out.println("1.On season");
+		System.out.println("2.Off season");
+		
+		
+		int s=sc.nextInt();
+		switch (s)
+		{
+		
+		case 1:
+				
 		super.discount();
+		break;
+		
+		case 2:
+			
 		d=(price*85)/100;
 		System.out.println("Price after 15% off season discount="+d);
+		break;
+		
+	    }
 	}
 
 	public static void main(String[] args) 
